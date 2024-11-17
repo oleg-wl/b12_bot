2e9dccde4495edc5391b067352ecb3ae1cfdca92
oleg_vl <@nitro-pc>
2024-11-17 20:55:55 +0300

    added isWeekend to calendar

--- database/masterdata.py
+++ database/masterdata.py
@@ -1,23 +1,52 @@
-from datetime import datetime, timedelta
+from calendar import weekheader
+from datetime import datetime, timedelta, date
 
-import polars as pl
-
-from tools import DB_Admin
-
-def insert_master_table():
-
-    seats = ["2B.{:0>3}".format(i) for i in range(1, 23)]
-
-    today = datetime.today()
-    date_list = [today + timedelta(days=i) for i in range(365)]
-    df = (
-        pl.DataFrame({"dates": date_list})
-        .join(pl.DataFrame({"seat": seats}), how="cross")
-        .with_column(pl.lit(value=None, dtype=pl.Float32).alias('user_id'))
-        .cast({"dates": pl.Date})
-        .write_database(table_name="mastertable", connection=DB_Admin().engine.connect(), if_table_exists='replace')
-    )
+import pandas as pd
+import requests
 
 
-if __name__ == "__main__":
-    insert_master_table()
+class MasterTable:
+
+    seats = None
+    master_table = None
+
+    def _make_seats_list(self):
+
+        # собрать лист с рабочими местами
+        # NOTE: кириллица
+        self.seats = ["2В.{:0>3}".format(i) for i in range(1, 23)]
+        self.seats.append("2А.001")
+        self.seats.append("2А.002")
+
+
+    def _make_weekends(self, year: int):
+        r = requests.get(f"https://n01.isdayoff.ru/api/getdata?year={year}&cc=ru&pre=0")
+        return [int(d) for d in r.text]
+
+    def _make_calendar_list(self, year: int):
+        # обьект календаря года с флагом 0 - рд/1 - вых
+        year = int(year)
+        weekend_days = self._make_weekends(year)
+
+        start_day = date(year=year, month=1, day=1)
+        end_day = date(year=year, month=12, day=31)
+
+        period = pd.date_range(start=start_day, end=end_day, freq="D")
+
+        return pd.DataFrame(period, columns=["period day"]).join(
+            pd.DataFrame(weekend_days, columns=["is_weekend"]), how="left"
+        )
+
+    def make_table(self, start_y: int, end_y: int):
+        try:
+            int(start_y)
+            int(end_y)
+        except TypeError:
+            print("Год должен быть интом")
+        else:
+
+            self._make_seats_list()
+
+            self.master_table = pd.concat(
+                [self._make_calendar_list(year=y) for y in range(start_y, end_y + 1)]
+            ).join(pd.DataFrame(self.seats), how="cross")
