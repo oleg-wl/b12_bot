01bddbe797de3b5b77c0a7e2a2dc22ab1e8d68a7
oleg_vl <@nitro-pc>
2025-02-27 10:12:28 +0300

    error handler fix 4095 logic

--- source/error_handler.py
+++ source/error_handler.py
@@ -28,12 +28,13 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
     update_str = update.to_dict() if isinstance(update, Update) else str(update)
     message = (
         "An exception was rised in bot \n"
-        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
-        "</pre>\n\n"
+     #   f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+     #   "</pre>\n\n"
         f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
         f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
         f"<pre>{html.escape(tb_string)}</pre>"
     )
+    if len(message) > 4095: message = message[-4095:] 
 
     # send the message
     await context.bot.send_message(
