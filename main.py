82e974ba54ac49a425ae26c72e0f59b8d5c693be
oleg_vl <@nitro-pc>
2025-09-03 22:42:00 +0300

    format fix

--- main.py
+++ main.py
@@ -7,16 +7,7 @@ from dotenv import load_dotenv
 
 from loguru import logger
 
-from telegram import (
-    Update
-    )
-
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    TypeHandler,
-    filters
-)
+from telegram.ext import ApplicationBuilder, CommandHandler, TypeHandler, filters
 
 from source.start import StartCommand
 from source.book import GROUP_CHAT_ID, BookCommand
@@ -24,16 +15,14 @@ from source.unbook import UnbookCommand
 from source.whos import WhosCommand
 
 from source.error_handler import error_handler
-from source.middleware import GrAuthMiddleware, ChatMembershipMiddleware
-
 
 load_dotenv("config.env")
-GROUP_CHAT_ID = os.getenv('GROUP_CHAT_ID')
+GROUP_CHAT_ID = os.getenv("GROUP_CHAT_ID")
+
 
 def main():
 
     token = os.getenv("BOT_API")
-    group_cheker = ChatMembershipMiddleware(GROUP_CHAT_ID)
 
     s = StartCommand()
     b = BookCommand()
@@ -41,27 +30,23 @@ def main():
     ws = WhosCommand()
 
     # увеличены таймауты если что-то с сетью
-    app = (
-        ApplicationBuilder()
-        .token(token)
-        .build()
-    )
+    app = ApplicationBuilder().token(token).build()
 
     start_handler = CommandHandler("start", s.start, filters=filters.COMMAND)
 
     book_seat_handler = CommandHandler("book", b.dates, filters=filters.COMMAND)
     book_seat_conv = b.conversation(entry=[book_seat_handler])
 
-    unbook_seat_handler = CommandHandler("myseats", ub.check_my_seats, filters=filters.COMMAND)
+    unbook_seat_handler = CommandHandler(
+        "myseats", ub.check_my_seats, filters=filters.COMMAND
+    )
     unbook_seat_conv = ub.conversation(entry=[unbook_seat_handler])
 
-    whos_handler = CommandHandler('whos', ws.whos_date, filters=filters.COMMAND)
+    whos_handler = CommandHandler("whos", ws.whos_date, filters=filters.COMMAND)
     whos_conv = ws.conversation(entry=[whos_handler])
 
     help_handler = CommandHandler("help", s.help, filters=filters.COMMAND)
 
-    # Добавляем middleware
-    # app.add_handler(TypeHandler(Update, group_cheker), group=1)
     app.add_handler(start_handler)
     app.add_handlers([book_seat_conv, unbook_seat_conv, whos_conv])
     app.add_handler(help_handler)
@@ -75,11 +60,7 @@ if __name__ == "__main__":
     fmt = "<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | <level>{level}</level> | {extra} - {message}"
 
     logger.remove()
-    logger.add(sys.stdout, 
-               format=fmt,
-               level="TRACE", 
-               colorize=True,
-               catch=True)
+    logger.add(sys.stdout, format=fmt, level="TRACE", colorize=True, catch=True)
 
     # лог в файл только инфо
     logger.add(
@@ -90,7 +71,7 @@ if __name__ == "__main__":
         rotation="2MB",
         backtrace=False,
         diagnose=False,
-        catch=False, 
+        catch=False,
     )
 
     # отдельный логгер для ошибок
@@ -102,7 +83,7 @@ if __name__ == "__main__":
         rotation="2MB",
         backtrace=True,
         diagnose=True,
-        catch=True, 
+        catch=True,
     )
 
     main()
--- source/start.py
+++ source/start.py
@@ -9,14 +9,12 @@ from loguru import logger
 from telegram.ext import (
     ContextTypes,
     ConversationHandler,
-    CommandHandler,
-    MessageHandler,
-    filters,
 )
 
 import database
 
 from .utils import KeyboardBuilder
+
 
 class CoreCommand(ABC):
     """
@@ -32,50 +30,58 @@ class CoreCommand(ABC):
     def __repr__(self):
 
         class_name = self.__class__.__name__
-        
-        attrs_lst = [f'{key}={value!r}' for key, value in self.__dict__.items() if not key.startswith('_')]
+
+        attrs_lst = [
+            f"{key}={value!r}"
+            for key, value in self.__dict__.items()
+            if not key.startswith("_")
+        ]
         attrs = ", ".join(attrs_lst)
-        
-        return f"{class_name}:attributes: {attrs}" 
+
+        return f"{class_name}:attributes: {attrs}"
 
     @classmethod
-    def _initialisation(cls, update: Update) -> tuple[str, str|int]:
+    def _initialisation(cls, update: Update) -> tuple[str, str | int]:
         """
         Функция возвращает юзернейм и чат айди и контекстный логгер
 
-        :param Update update: update 
-        :return : _username, _chat_id, context_logger 
+        :param Update update: update
+        :return : _username, _chat_id, context_logger
         """
 
         _username: str = update.effective_chat.username
-        _chat_id: str|int = update.effective_chat.id
+        _chat_id: str | int = update.effective_chat.id
 
         # аттрибут контекстного логгера для логгирования юзернейма и чат айди
-        context_logger = logger.bind(username = _username, chat_id = _chat_id)
+        context_logger = logger.bind(username=_username, chat_id=_chat_id)
 
         return _username, _chat_id, context_logger
 
     @classmethod
-    def _json_callback(cls, query: CallbackQuery) -> tuple[str|None, int|None] | None:
+    def _json_callback(
+        cls, query: CallbackQuery
+    ) -> tuple[str | None, int | None] | None:
         """
         в CallbackQuery всегда json!
         метод десериализует json из callback_query
 
         :param CallbackQuery query: команда в action
-        :return tuple[str|None, int|None]: i  
+        :return tuple[str|None, int|None]: i
         """
         s_data: dict = json.loads(query.data)
 
-        action: str | None = str(s_data.get('action')) if s_data.get('action') else None
-        i: int | None = int(s_data.get('i')) if s_data.get('i') else None
+        action: str | None = str(s_data.get("action")) if s_data.get("action") else None
+        i: int | None = int(s_data.get("i")) if s_data.get("i") else None
 
         return action, i
-        
+
     @abstractmethod
     def conversation(self, entry):
         pass
 
-    async def _check_group(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    async def _check_group(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> bool:
         """
         Метод для проверки, что запущена команда бота в групповом чате. Заглушка чтобы не пускать в групповой чат спамить
 
@@ -86,48 +92,54 @@ class CoreCommand(ABC):
         :return bool: 1 - если бот запущен в группе, 0 - если бот в личном чате
         """
 
-        if update.effective_chat.type in ['group', 'supergroup']:
+        if update.effective_chat.type in ["group", "supergroup"]:
             message_id = update.message.message_id
 
-            kb = [[InlineKeyboardButton('Написать боту', url = 'tg://user?id={}'.format(context.bot.id))]]
+            kb = [
+                [
+                    InlineKeyboardButton(
+                        "Написать боту", url="tg://user?id={}".format(context.bot.id)
+                    )
+                ]
+            ]
 
             await update.message.reply_text(
                 reply_to_message_id=message_id,
-                text = 'Привет {}. Для брони мест напиши мне в личные сообщения'.format(update.message.from_user.name),
-                reply_markup = InlineKeyboardMarkup(kb)
-
+                text="Привет {}. Для брони мест напиши мне в личные сообщения".format(
+                    update.message.from_user.name
+                ),
+                reply_markup=InlineKeyboardMarkup(kb),
             )
-            logger.info('попытка написать в групповой чат {}', message_id)
+            logger.info("попытка написать в групповой чат {}", message_id)
             return True
-        
-        
+
         member = await context.bot.get_chat_member(
-            chat_id=os.getenv('GROUP_CHAT_ID'), 
-            user_id=update.effective_user.id
+            chat_id=os.getenv("GROUP_CHAT_ID"), user_id=update.effective_user.id
         )
 
-        if member.status == 'left':
-            logger.info(f'user: {update.effective_user.id}, member: {member.status}')
+        if member.status == "left":
+            logger.info(f"user: {update.effective_user.id}, member: {member.status}")
             return True
 
         return False
 
-    
-    async def cancel_conversation(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        # для fallbascks     
+    async def cancel_conversation(
+        self, update: Update, context: ContextTypes.DEFAULT_TYPE
+    ):
+        # для fallbascks
         _message = update.message
-        
+
         query = update.callback_query
         await query.answer()
 
-        if _message.caption: #проверить если сообщение с картинкой
-            
-            await query.edit_message_caption('Диалог завершен')
+        if _message.caption:  # проверить если сообщение с картинкой
 
-        elif _message.text:  #проверить если в сообщении текст
+            await query.edit_message_caption("Диалог завершен")
 
-            await query.edit_message_text('Диалог завершен')
-            
+        elif _message.text:  # проверить если в сообщении текст
+
+            await query.edit_message_text("Диалог завершен")
+
         return ConversationHandler.END
 
 
@@ -144,26 +156,27 @@ class StartCommand(CoreCommand):
 
         _username, _chat_id, context_logger = self._initialisation(update=update)
 
-        _check_group_chat: int | None = await self._check_group(update=update, context=context)
+        _check_group_chat: int | None = await self._check_group(
+            update=update, context=context
+        )
         if _check_group_chat:
             return ConversationHandler.END
 
         # проверить, что юзер уже есть в БД по чатайди
         user = database.check_user_chat_id(engine=database.engine, chat_id=_chat_id)
         member = await context.bot.get_chat_member(
-            chat_id=os.getenv('GROUP_CHAT_ID'), 
-            user_id=_chat_id
+            chat_id=os.getenv("GROUP_CHAT_ID"), user_id=_chat_id
         )
-        logger.info(f'user: {_username}, member: {member.status}')
-        if user and member.status !='left':
-            context_logger.success('/Start command in private chat')
+        logger.info(f"user: {_username}, member: {member.status}")
+        if user and member.status != "left":
+            context_logger.success("/Start command in private chat")
             await context.bot.send_message(
                 chat_id=_chat_id,
                 text=f"Привет {_username}, рад снова видеть тебя.\n/book - для бронирования места\n/myseats - твои места\n/whois - посмотреть кто занял место",
             )
 
         # если юзера нет попросить пароль
-        elif user is None and member.status != 'left':
+        elif user is None and member.status != "left":
             try:
                 now = datetime.datetime.now()
 
@@ -181,34 +194,38 @@ class StartCommand(CoreCommand):
                     lastname=last_name,
                     created_at=now,
                 )
-                logger.success('Password correct. chat_id:{}, username:{}, firstname:{} inserted', _chat_id, username, first_name)
+                logger.success(
+                    "Password correct. chat_id:{}, username:{}, firstname:{} inserted",
+                    _chat_id,
+                    username,
+                    first_name,
+                )
 
-                logger.success('/Start in private chat - try auth')
+                logger.success("/Start in private chat - try auth")
                 await context.bot.send_message(
                     chat_id=_chat_id,
                     text=f"Привет {_username}, ты есть в чате, можешь пользоваться ботом",
                 )
-            
+
             except Exception as e:
                 logger.exception(e)
                 await context.bot.send_message(chat_id=_chat_id, text="some error")
-        else: 
-            context_logger.info('попытка доступа без прав')
-            await context.bot.send_message(
-                chat_id=_chat_id,
-                text='sorry'
-            )
+        else:
+            context_logger.info("попытка доступа без прав")
+            await context.bot.send_message(chat_id=_chat_id, text="sorry")
+
     def conversation(self, entry):
-        #dummy
+        # dummy
         return super().conversation(entry)
 
-    
     async def help(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-        _check_group_chat: int | None = await self._check_group(update=update, context=context)
+        _check_group_chat: int | None = await self._check_group(
+            update=update, context=context
+        )
 
         if not _check_group_chat:
-        
+
             await update.message.reply_text(
-                text='Бот для брони места в офисе - Невская ратуша, корп4 эт2\nМеста: 2В.001, 2В.007,2В.008, 2В.011, 2В.012, 2А.002, 2А.003 забронены по дефолту. Для брони /book: лимит три дня. если бронить разные места внутри дня, бронь перезаписывается - 1 день - 1 место\nТвои активные брони на ближайшие 5 дней: /myseats\nЕсли надо оптом снять бронь (отпуск или заболел) напиши @lordcrabov'
+                text="Бот для брони места в офисе - Невская ратуша, корп4 эт2\nМеста: 2В.001, 2В.007,2В.008, 2В.011, 2В.012, 2А.002, 2А.003 забронены по дефолту. Для брони /book: лимит три дня. если бронить разные места внутри дня, бронь перезаписывается - 1 день - 1 место\nТвои активные брони на ближайшие 5 дней: /myseats\nЕсли надо оптом снять бронь (отпуск или заболел) напиши @lordcrabov"
             )
